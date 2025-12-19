0f2c8770883704de0d0f7608fb9b8a31d9031dc4
swatianokhi <swati.anokhi@snabbtech.com>
2025-12-19 11:47:32 +0530

    fix

--- src/utils/pointsCalculator.js
+++ src/utils/pointsCalculator.js
@@ -40,7 +40,8 @@ function calculateTotalPoints(metrics) {
   // New rules:
   // - Present points include Late, Medical, Substitute counts
   // - TYFCB: 1 point per 1000 rupees
-  const presentPoints = (P + L + M + S) * 2;
+  const presentPoints = (P + L + S) * 2;
+  const medicalPoints = M * -2;
   const absentPoints = A * -2;
   const referralPoints = (RGI + RGO + RRI + RRO) * 5;
   const visitorPoints = V * 10;
@@ -51,6 +52,7 @@ function calculateTotalPoints(metrics) {
 
   const total =
     presentPoints +
+    medicalPoints +
     absentPoints +
     referralPoints +
     visitorPoints +
